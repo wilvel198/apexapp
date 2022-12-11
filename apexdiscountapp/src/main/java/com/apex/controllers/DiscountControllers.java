41d3fbcb2cb60c229fdba96a517457782c61bf29
thewatcher198 <wvelasquezcorp@gmail.com>
2022-12-10 19:36:46 -0600

    junit updates

--- apexdiscountapp/src/main/java/com/apex/controllers/DiscountControllers.java
+++ apexdiscountapp/src/main/java/com/apex/controllers/DiscountControllers.java
@@ -1,18 +1,13 @@
 package com.apex.controllers;
 
-import java.net.http.HttpResponse.ResponseInfo;
-
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.apex.models.ResponseValue;
 import com.apex.models.TransactionInfo;
-
 import discountlogic.ApexPointLogic;
 
 @RestController
